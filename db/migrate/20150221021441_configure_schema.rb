abed3711261cc93b0e46f247a73499611c707434
Ignacio <imonge@growthaccelerationpartners.com>
2015-03-22 19:05:38 -0600

    Comment role and schema migration

--- db/migrate/20150221021441_configure_schema.rb
+++ db/migrate/20150221021441_configure_schema.rb
@@ -1,7 +1,7 @@
 class ConfigureSchema < ActiveRecord::Migration
   def change
-    execute 'CREATE SCHEMA r24e'
-    execute 'CREATE ROLE r24e'
-    execute 'ALTER USER r24e SET search_path TO r24e,public'
+    #execute 'CREATE SCHEMA r24e'
+    #execute 'CREATE ROLE r24e'
+    #execute 'ALTER USER r24e SET search_path TO r24e,public'
   end
 end
